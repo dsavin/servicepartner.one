f95e62a653af177b1cd42de21112daf3caa06990
Dmytro Savin <savin.dmitry@gmail.com>
2018-09-20 08:31:32 +0200

    API: Add CORS and body parser

--- server/app.js
+++ server/app.js
@@ -2,12 +2,15 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
-
+const cors = require('cors')
+const bodyParser = require('body-parser')
 const indexRouter = require('./routes/index');
 
 const app = express();
 
 app.use(logger('dev'));
+app.use(cors());
+app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
