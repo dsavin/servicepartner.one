1915634beac36ee1de68c582d29ac0678f5a522b
Dmytro Savin <savin.dmitry@gmail.com>
2018-09-20 10:04:01 +0200

    API: Add single senior case

--- server/routes/index.js
+++ server/routes/index.js
@@ -12,11 +12,12 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/optimize', function(req, res, next) {
+  const body = req.body;
   // input data validation
   // validation: rooms property
   debug('validation started');
-  if (req.body.hasOwnProperty('rooms') && req.body.rooms.length > 0) {
-    if (req.body.rooms.every((room) => typeof room === 'number') === false) {
+  if (body.hasOwnProperty('rooms') && body.rooms.length > 0) {
+    if (body.rooms.every((room) => typeof room === 'number') === false) {
       debug('validation failed for rooms');
       res.status(400);
       res.json({'Validation Error': `At least one element of array in 'rooms' property is not a number`});
@@ -29,23 +30,36 @@ router.post('/optimize', function(req, res, next) {
   }
 
   // validation: senior property
-  if (req.body.hasOwnProperty('senior') === false
-    || typeof req.body.senior !== 'number') {
+  if (body.hasOwnProperty('senior') === false
+    || typeof body.senior !== 'number') {
     debug('validation failed for senior');
     res.status(400);
     res.json({'Validation Error': `'senior' property is missing or not a number`});
   }
 
   // validation: junior property
-  if (req.body.hasOwnProperty('junior') === false
-    || typeof req.body.junior !== 'number') {
+  if (body.hasOwnProperty('junior') === false
+    || typeof body.junior !== 'number') {
     debug('validation failed for junior');
     res.status(400);
     res.json({'Validation Error': `'junior' property is missing or not a number`});
   }
 
   // everything is fine, we can proceed to business logic
+  // going through rooms
+  let response = [];
+  debug('loop through rooms array');
+  body.rooms.forEach((room) => {
+    if (room > body.senior) {
 
+    } else {
+      debug('singe senior is needed');
+      response.push({
+        "senior": 1
+      });
+    }
+  });
+  res.send(response);
 
 });
 
