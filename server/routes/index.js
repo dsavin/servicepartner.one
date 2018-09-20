e4308581e16ec6a525a5d25dbb2357acbe8462e5
Dmytro Savin <savin.dmitry@gmail.com>
2018-09-20 08:42:10 +0200

    API: Add health check route logic

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send(
+    {
+      healthCheck: "ok",
+      uptime: `${process.uptime()} sec`
+    });
+});
+
+router.get('/', function(req, res, next) {
+  res.send(
+    {
+      healthCheck: "ok",
+      uptime: `${process.uptime()} sec`
+    });
 });
 
 module.exports = router;
