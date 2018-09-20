22fe6c502ad8a37a642173c374b4247a694192da
Dmytro Savin <savin.dmitry@gmail.com>
2018-09-20 09:53:13 +0200

    API: Add logger

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const debug = require('debug')('server:api');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,13 +14,16 @@ router.get('/', function(req, res, next) {
 router.post('/optimize', function(req, res, next) {
   // input data validation
   // validation: rooms property
+  debug('validation started');
   if (req.body.hasOwnProperty('rooms') && req.body.rooms.length > 0) {
     if (req.body.rooms.every((room) => typeof room === 'number') === false) {
+      debug('validation failed for rooms');
       res.status(400);
       res.json({'Validation Error': `At least one element of array in 'rooms' property is not a number`});
     }
   } else {
     // missing rooms property
+    debug('validation failed for rooms');
     res.status(400);
     res.json({'Validation Error': `'rooms' property is missing or empty`});
   }
@@ -27,6 +31,7 @@ router.post('/optimize', function(req, res, next) {
   // validation: senior property
   if (req.body.hasOwnProperty('senior') === false
     || typeof req.body.senior !== 'number') {
+    debug('validation failed for senior');
     res.status(400);
     res.json({'Validation Error': `'senior' property is missing or not a number`});
   }
@@ -34,6 +39,7 @@ router.post('/optimize', function(req, res, next) {
   // validation: junior property
   if (req.body.hasOwnProperty('junior') === false
     || typeof req.body.junior !== 'number') {
+    debug('validation failed for junior');
     res.status(400);
     res.json({'Validation Error': `'junior' property is missing or not a number`});
   }
